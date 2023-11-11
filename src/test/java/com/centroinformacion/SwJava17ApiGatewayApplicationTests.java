b2363e79054f5c5ccf7156ff32c9f548d79ce122
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-11-11 18:49:12 -0500

    inicio

---
+++ src/test/java/com/centroinformacion/SwJava17ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centroinformacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwJava17ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
